abec73fb693aa95203b2d0cfdd1f165f28c2edff
寂寞沙洲冷 <59199651+1638276310@users.noreply.github.com>
2025-04-18 02:34:55 +0800

    改为不撤回提示

--- apps/MagnetLinkMao.js
+++ apps/MagnetLinkMao.js
@@ -37,7 +37,7 @@ export class MagnetLink extends plugin {
         // 给msg_id赋值
         const msg_id = res.message_id;
         //指定撤回消息
-        await e.group.recallMsg(msg_id, { recallMsg: 5000 });
+        await e.group.recallMsg(msg_id);
         // await this.reply('正在搜索，请稍等...',{ recallMsg: 100 });
         let match = e.msg.match(/^#?磁力猫\s*(\S+)(\s+(\S+))?(\s+(\S+))?(\s+(\d+))?$/);
         if (!match) {
