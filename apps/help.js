3a9101c874e010715aaa0dd43732f0bad08c89cd
寂寞沙洲冷 <59199651+1638276310@users.noreply.github.com>
2025-05-12 03:43:44 +0800

    更新帮助图

--- apps/help.js
+++ apps/help.js
@@ -17,7 +17,7 @@ export class sphelp extends plugin {
     }
     async sp_help(e) {
         await this.reply(
-                    await e.reply([segment.image("./plugins/sp-plugin/config/help.png"), "涩批帮助"])
+                    await e.reply([segment.image("https://ps.ssl.qhimg.com/t027f53f1cdfbdeef2c.jpg")])
         )
     }
 }
