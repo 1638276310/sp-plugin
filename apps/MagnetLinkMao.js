2c1a486cdbb84bddb50c919ed2cdea99f7aba1cd
寂寞沙洲冷 <59199651+1638276310@users.noreply.github.com>
2025-05-28 21:56:06 +0800

    更新磁力猫timeout

--- apps/MagnetLinkMao.js
+++ apps/MagnetLinkMao.js
@@ -64,7 +64,7 @@ export class MagnetLink extends plugin {
                 await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
                 await page.goto(urls[i], { 
                     waitUntil: 'domcontentloaded', 
-                    timeout: 10000 
+                    timeout: 60000 
                 });
                 
                 // 等待结果加载
