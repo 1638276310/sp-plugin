a8461d75b24b740c87f8f4a747fa3a825f9ea29d
寂寞沙洲冷 <1638276310@qq.com>
2025-10-02 15:24:17 +0800

    更新UA

--- apps/mzt.js
+++ apps/mzt.js
@@ -412,7 +412,7 @@ export class mztPlugin extends plugin {
       });
       const page = await browser.newPage();
       await page.setUserAgent(
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
       );
 
       // 导航到目标页面
