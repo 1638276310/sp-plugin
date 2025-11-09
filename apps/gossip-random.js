f9f67d0e2b420174e60c5881174d72b4af416343
寂寞沙洲冷 <1638276310@qq.com>
2025-11-09 16:58:54 +0800

    修复导入错误，傻鸟AI

--- apps/gossip-random.js
+++ apps/gossip-random.js
@@ -4,7 +4,6 @@
  * @author AI-Assistant
  */
 
-import plugin from "../../lib/plugins/plugin.js";
 import { readIds } from "../lib/gossip-utils.js";
 import { fetchVideoById } from "../lib/gossip-utils.js";
 
