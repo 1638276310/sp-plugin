65afedd125a58f0adda7272894eed131e1f6f5da
寂寞沙洲冷 <1638276310@qq.com>
2025-11-24 19:05:03 +0800

    漏删，修复23图URL

--- apps/tu.js
+++ apps/tu.js
@@ -59,12 +59,12 @@ export class CosImageFetcher extends plugin {
   }
 
   async process2Images(e) {
-    const url = `https://www.mengxix.top/acg/index.php`;
+    const url = `https://www.mengxix.top/TRSS/acg/index.php`;
     await this.sendImages(e, url);
   }
 
   async process3Images(e) {
-    const url = `https://www.mengxix.top/reality/index.php?type=img`;
+    const url = `https://www.mengxix.top/TRSS/reality/index.php?type=img`;
     await this.sendImages(e, url);
   }
 
