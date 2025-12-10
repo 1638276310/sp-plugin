be247d6395f7b6aef59dfaea4e26a67502196cf3
寂寞沙洲冷 <1638276310@qq.com>
2025-12-11 00:56:23 +0800

    tag图改为10个

--- apps/tag.js
+++ apps/tag.js
@@ -57,7 +57,7 @@ export class SetuImageFetcher extends plugin {
     );
     const num = parseInt(numStr);
 
-    if (num > 20) {
+    if (num > 10) {
       await e.reply("你想冲死吗？");
       return;
     }
